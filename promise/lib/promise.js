7b9d6269efcc5a6f6c1c33e5069b1a3cdb529cfa
antqi <hi.antqi@gmail.com>
2020-05-25 15:38:17 +0800

    完成自定义Promise的整体结构定义

--- promise/lib/promise.js
+++ promise/lib/promise.js
@@ -1,3 +1,61 @@
-/**
- * 定义Promise函数模块 (ES5)
+/*
+ * @Author: antqi
+ * @Email: hi.antqi@gmail.com
+ * @Date: 2020-05-25 15:14:05
+ * @Last Modified by: antqi
+ * @Last Modified time: 2020-05-25 15:37:45
+ * @Description: Description
  */
+
+;(function (param) {
+  function Promise(excotor) {
+    function resolve() {}
+
+    function reject() {}
+    // excotor，执行器函数
+    excotor(resolve, reject)
+  }
+
+  /**
+   * @desc 指定成功与失败的回调函数
+   * @param {Function} onResolved 成功的回调函数
+   * @param {Function} onRejected 失败的回调函数
+   * @return Promise对象
+   */
+  Promise.prototype.then = function (onResolved, onRejected) {}
+
+  /**
+   * @desc 指定失败的回调函数
+   * @param {Function} onRejected 失败的回调函数
+   * @return Promise对象
+   */
+  Promise.prototype.catch = function (onRejected) {}
+
+  /**
+   * @desc 静态方法resolve，理解为快捷指定成功回调函数的方法
+   * @param {any} value 指定任意返回的值
+   * @return  被解析过Promise对象
+   */
+  Promise.resolve = function (value) {}
+
+  /**
+   * @desc 静态方法reject，理解为快捷指定失败回调函数的方法
+   * @param {any} reason 指定任意返回的值
+   * @return  带有特定被拒绝原因的Promise对象
+   */
+  Promise.resolve = function (reason) {}
+
+  /**
+   * @desc 启动多个异步任务并发运行 ，并组合返回的结果
+   * @param {Array｜String} promises 多个promise或值组成的数组
+   * @return 一个新的Promise，只有所有的Promise都成功才成功，只要有一个失败了就直接失败
+   */
+  Promise.all = function (promises) {}
+
+  /**
+   * @desc 启动多个异步任务并发运行 ，第一个解决或拒绝的promise的结果状态组成的新的promise
+   * @param {Array｜String} promises 多个promise或值组成的数组
+   * @return 一个新的promise ，第一个解决或拒绝的promise的结果状态就是最终的结果状态
+   */
+  Promise.race = function (promises) {}
+})(window)
