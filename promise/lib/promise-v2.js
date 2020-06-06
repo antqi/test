4836c3f7420e313e189fc2a0969b9d8e5e01aed3
antqi <hi.antqi@gmail.com>
2020-06-06 17:36:43 +0800

    refactor: 构造函数，处理回调部分

--- promise/lib/promise-v2.js
+++ promise/lib/promise-v2.js
@@ -3,24 +3,62 @@
  * @Email: hi.antqi@gmail.com
  * @Date: 2020-06-06 16:46:06
  * @Last Modified by: antqi
- * @Last Modified time: 2020-06-06 17:18:36
- * @Description: promise-version 2
+ * @Last Modified time: 2020-06-06 17:35:49
+ * @Description: promise-version 2 for broswer
  */
 
 !(function (env) {
   function Promise(executor) {
+    const _self = this
     // 同步执行executor
-    this.STATUS = {
+    _self.STATUS = {
       PENDING: 'pending',
       FULFILLED: 'fulfilled',
       REJECTED: 'rejected',
     }
-    this.status = this.STATUS.PENDING // 初始化状态
-    this.data = null // 结果值
-    this.callbackQueue = [] // 回调函数队列
+    _self.status = _self.STATUS.PENDING // 初始化状态
+    _self.data = null // 结果值
+    _self.callbackQueue = [] // 回调函数队列
 
-    function resolve() {}
-    function reject() {}
+    function resolve(value) {
+      // 只处理FULFILLED，并只处理一次
+      if (_self.status !== _self.STATUS.FULFILLED) {
+        return
+      }
+
+      //修改状态
+      _self.status = _self.STATUS.FULFILLED
+      //保存值
+      _self.data = value
+
+      for (let i = 0; i < _self.callbackQueue.length; i++) {
+        if (_self.callbackQueue[i].onFulfilled) {
+          setTimeout(function () {
+            _self.callbackQueue.onFulfilled(_self.data)
+          }, 0)
+        }
+      }
+    }
+
+    function reject() {
+      // 只处理REJECTED，并只处理一次
+      if (_self.status !== _self.STATUS.REJECTED) {
+        return
+      }
+
+      //修改状态
+      _self.status = _self.STATUS.REJECTED
+      //保存值
+      _self.data = value
+
+      for (let i = 0; i < _self.callbackQueue.length; i++) {
+        if (_self.callbackQueue[i].onRejected) {
+          setTimeout(function () {
+            _self.callbackQueue.onRejected(_self.data)
+          }, 0)
+        }
+      }
+    }
 
     try {
       executor(resolve, reject)
