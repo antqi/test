528b3d1356299ea73e0b20af5534c33c47a4b1e2
antqi <hi.antqi@gmail.com>
2020-06-10 15:09:44 +0800

    test: 浏览器的跨域限制

--- http/server-10000.js
+++ http/server-10000.js
@@ -5,6 +5,9 @@ http
     console.log(request.url)
     response.writeHead(200, {
       'Access-Control-Allow-Origin': 'http://localhost:9999',
+      'Access-Control-Allow-Methods': 'PUT',
+      'Access-Control-Allow-Headers': 'X-TEST-CORS',
+      'Access-Control-Max-Age': 1000,
     })
     response.end('来自端口：10000的消息')
   })
