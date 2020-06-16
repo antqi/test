53c4b560f7c3116ca5a2a40b370750693f6dd6cd
antqi <hi.antqi@gmail.com>
2020-06-16 16:33:15 +0800

    test: hTTP 缓存头测试

--- http/server-10000.js
+++ http/server-10000.js
@@ -2,13 +2,15 @@ const http = require('http')
 
 http
   .createServer(function (request, response) {
-    console.log(request.url)
-    response.writeHead(200, {
-      'Access-Control-Allow-Origin': 'http://localhost:9999',
-      'Access-Control-Allow-Methods': 'PUT',
-      'Access-Control-Allow-Headers': 'X-TEST-CORS',
-      'Access-Control-Max-Age': 1000,
-    })
+    console.log(request.headers['if-modified-since'])
+    const expiresSign = 111
+    if (request.headers['if-modified-since'] === expiresSign)
+      response.writeHead(200, {
+        'Access-Control-Allow-Origin': 'http://localhost:9999',
+        'Access-Control-Allow-Methods': 'PUT',
+        'Access-Control-Allow-Headers': 'If-Modified-Since',
+        'Access-Control-Max-Age': 1000,
+      })
     response.end('来自端口：10000的消息')
   })
   .listen(10000)
--- http/server.js
+++ http/server.js
@@ -4,7 +4,35 @@ const fs = require('fs')
 http
   .createServer(function (request, response) {
     console.log(request.url)
-    const html = fs.readFileSync(__dirname + '/test.html', 'UTF-8')
-    response.end(html)
+
+    if (request.url === '/') {
+      const html = fs.readFileSync(__dirname + '/test.html', 'UTF-8')
+      response.writeHead(200, {
+        'Content-Type': 'text/html',
+      })
+      response.end(html)
+    } else if (request.url === '/main.js') {
+      if (request.headers['if-modified-since'] === '1234') {
+        response.writeHead(304, {
+          'Content-Type': 'text/javascript',
+          // Expires: new Date(Date.now() + 1000 * 30), // 设置缓存过期时间，HTTP/1.0中出现
+          'Cache-Control': 'max-age=30000', // 多少秒之后过期
+          // 'Cache-Control': 'max-age=300,no-cache',
+          'Last-Modified': '1234',
+          // Etag: '5678',
+        })
+        response.end('console.log("main.js ~---~~")')
+      } else {
+        response.writeHead(200, {
+          'Content-Type': 'text/javascript',
+          // Expires: new Date(Date.now() + 1000 * 30), // 设置缓存过期时间，HTTP/1.0中出现
+          'Cache-Control': 'max-age=30000', // 多少秒之后过期
+          // 'Cache-Control': 'max-age=300,no-cache',
+          'Last-Modified': '1234',
+          // Etag: '5678',
+        })
+        response.end('console.log("main.js ~---~~")')
+      }
+    }
   })
   .listen(9999)
