abe9af7bafac6587c476dd809106df3cb8a70954
antqi <hi.antqi@gmail.com>
2020-06-23 22:48:03 +0800

    test: commonjs-node

---
+++ JavaScript模块化/5-CommonJS/Node/app.js
@@ -0,0 +1,6 @@
+const module1 = require('./modules/module1')
+const module2 = require('./modules/module2')
+const module3 = require('./modules/module3')
+
+module1(module2.num1, module2.num2)
+module3()
---
+++ JavaScript模块化/5-CommonJS/Node/modules/module1.js
@@ -0,0 +1,12 @@
+/**
+ * 两数相加
+ * @param {Number} x
+ * @param {Number} y
+ * @return x+y
+ */
+function add(x, y) {
+  console.log('run module2 add()')
+  return x + y
+}
+
+module.exports = add
